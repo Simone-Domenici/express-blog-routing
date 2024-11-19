d348e111d4ae18f2bb0fd855f7b7b6f5f6cc632c
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-19 16:02:26 +0100

    ADD index and show responses in json

--- routers/posts.js
+++ routers/posts.js
@@ -4,27 +4,34 @@ const posts = require('../posts.js')
 
 // Index
 router.get('/' ,(req,res) => {
-    res.send('Lista dei post')
+    console.log('Lista dei post')
+    res.json(posts)
 })
 // Show
-router.get('/:id' ,(req,res) => {
-    res.send('Post all\'id #')
+router.get('/:slug' , (req,res) => {
+    const slug = req.params.slug
+	console.log(`Post con lo slug:${slug}`)
+
+	const post = posts.find((p) => p.slug === slug)
+
+	res.json(post)
 })
 // Create
 router.post('/' , (req,res) => {
     res.send('Creazione del post')
 })
 // Update
-router.put('/:id' , (req,res) => {
-    res.send('Aggiornamento del post')
+router.put('/:slug' , (req,res) => {
+    const slug = req.params.slug
+    res.send(`Aggiornamento del post ${slug}`)
 })
 // Modify
-router.patch('/:id' , (req,res) => {
-    res.send('Modifica del post')
+router.patch('/:slug' , (req,res) => {
+    res.send(`Modifica del post ${slug}`)
 })
 // Delete
-router.delete('/:id' , (req,res) => {
-    res.send('Cancellazione del post')
+router.delete('/:slug' , (req,res) => {
+    res.send(`Cancellazione del post ${slug}`)
 })
 
 module.exports = router
